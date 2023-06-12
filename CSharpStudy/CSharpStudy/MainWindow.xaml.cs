2e4b31a6557cef7a1070df3ccf4052f9553f91a5
hyunwoo <junghw86@quantamatrix.com>
2023-06-12 17:35:18 +0900

    fix 불필요한 button event 삭제

--- CSharpStudy/CSharpStudy/MainWindow.xaml.cs
+++ CSharpStudy/CSharpStudy/MainWindow.xaml.cs
@@ -24,10 +24,5 @@ namespace CSharpStudy
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
