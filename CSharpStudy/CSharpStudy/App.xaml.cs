39b231833bb39f82174f136521f8ef31d090be34
codingHate <85850800+CodingHate@users.noreply.github.com>
2023-06-15 08:51:24 +0900

    Add files via upload

--- CSharpStudy/CSharpStudy/App.xaml.cs
+++ CSharpStudy/CSharpStudy/App.xaml.cs
@@ -1,17 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Primitives;
+using Prism.Events;
+using System.Text.Json;
+using System.Threading.Tasks.Dataflow;
 using System.Windows;
 
-namespace CSharpStudy
+namespace OptionTestProject
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public IHost HostObject { get; }
+
+        public IEventAggregator EventAggregator;
+
+        public BroadcastBlock<ChangeNotifyType> ChangeNotifyBroadcastBlock { get; }
+
+        public App()
+        {
+            ChangeNotifyBroadcastBlock = new BroadcastBlock<ChangeNotifyType>(null);
+
+            HostObject = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, configuration) =>
+                {
+                    configuration.Sources.Clear();
+                    IConfigurationRoot configurationRoot = configuration
+                        .SetBasePath(context.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile("default.json", optional: false, reloadOnChange: true)
+                        .Build();
+
+                    _ = ChangeToken.OnChange(() => configurationRoot.GetSection("AppSettings").GetReloadToken(), () =>
+                    {
+                        ChangeNotifyBroadcastBlock.Post(ChangeNotifyType.APP_SETTINGS);
+                    });
+                    _ = ChangeToken.OnChange(() => configurationRoot.GetSection("defaultSquence").GetReloadToken(), () =>
+                    {
+                        ChangeNotifyBroadcastBlock.Post(ChangeNotifyType.DEFAULT);
+                    });
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    _ = services.Configure<AppSettings>(context.Configuration.GetSection("AppSettings"));
+                    _ = services.Configure<DefaultSequence>(context.Configuration.GetSection("defaultSquence"));
+                    _ = services.AddTransient(typeof(MainWindow));
+                }).Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            await HostObject.StartAsync();
+            HostObject.Services.GetRequiredService<MainWindow>().Show();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            await HostObject.StopAsync();
+        }
     }
 }
--- CSharpStudy/CSharpStudy/MainWindow.xaml.cs
+++ CSharpStudy/CSharpStudy/MainWindow.xaml.cs
@@ -1,28 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks.Dataflow;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace CSharpStudy
+namespace OptionTestProject
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private IOptionsSnapshot<AppSettings> appSettings { get; }
+
+        private IOptionsSnapshot<DefaultSequence> defaultSequenceSettings { get; }
+
+        private IDisposable broadcastLink { get; set; }
+
+        public MainWindow(IOptionsSnapshot<AppSettings> appSettings, IOptionsSnapshot<DefaultSequence> defaultSequenceSettings)
         {
+            this.appSettings = appSettings;
+            this.defaultSequenceSettings = defaultSequenceSettings;
+/*            this.configuration.OnChange((settings) =>
+            {
+                var a1a33 = this.configuration;
+                var i = 0;
+            });*/
             InitializeComponent();
+            SetSubscribeChangegNotifyBroadcastBlock();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                TextBox control = sender as TextBox;
+                appSettings.Value.Name = control.Text;
+                defaultSequenceSettings.Value.addNewSample.description = control.Text;
+                AppSettings.SaveChanged(appSettings.Value);
+                DefaultSequence.SaveChanged(defaultSequenceSettings.Value);
+            }
+        }
+
+        private void SetSubscribeChangegNotifyBroadcastBlock()
+        {
+            if (Application.Current is App rootApp)
+            {
+                broadcastLink = rootApp.ChangeNotifyBroadcastBlock.LinkTo(new ActionBlock<ChangeNotifyType>(type =>
+                {
+                    switch (type)
+                    {
+                        case ChangeNotifyType.INVALID:
+                            break;
+                        case ChangeNotifyType.APP_SETTINGS:
+                            Dispatcher.Invoke(() =>
+                            {
+                                TextBlock.Text = appSettings.Value.Name;
+                            });
+                            break;
+                        case ChangeNotifyType.DEFAULT:
+                            Dispatcher.Invoke(() =>
+                            {
+                                TextBlock1.Text = defaultSequenceSettings.Value.addNewSample.description;
+                            });
+                            break;
+                        default:
+                            break;
+                    }
+                }));
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            broadcastLink.Dispose();
         }
     }
 }
